50f923301bfddfce8d5c76755181b5c2ea2a5898
CristhianKauan <cristhiandev20@gmail.com>
2023-01-28 16:56:51 -0300

    a

--- script.js
+++ script.js
@@ -22,63 +22,30 @@ else {
 c++
 }
 
-function trocar(){
+function trocar(x){
 
-  var axolote= documente.getElementById('axolote')
-  var leao= documente.getElementById('leao')
-  var panda= documente.getElementById('panda')
-  var cavalo= documente.getElementById('cavalo')
-  var coruja = documente.getElementById('coruja')
-  var aranha = documente.getElementById('aranha')
-  var abelha = documente.getElementById('abelha')
-  var  arraia= documente.getElementById('arraia')
-  var coelho= documente.getElementById('coelho')
+  
+
+  var axolote= document.getElementById('axolote')
+  var leao= document.getElementById('leao')
+
   switch (x){
 
     case 2:
       axolote.classList.add('ativo')
       axolote.classList.remove('desativo')
+
+      leao.classList.add('desativo')
+      leao.classList.remove('ativo')
       break
 
       case 3:
         window.alert('aaaaaaaaaaa')
         leao.classList.add('ativo')
         leao.classList.remove('desativo')
-      break
 
-      case 4:
-        panda.classList.add('ativo')
-        panda.classList.remove('desativo')
-      break
-
-      case 5:
-        cavalo.classList.add('ativo')
-        cavalo.classList.remove('desativo')
-      break
-
-      case 6:
-        coelho.classList.add('ativo')
-        coelho.classList.remove('desativo')
-      break
-
-      case 7:
-        aranha.classList.add('ativo')
-        aranha.classList.remove('desativo')
-      break
-
-      case 8:
-        abelha.classList.add('ativo')
-        abelha.classList.remove('desativo')
-      break
-
-      case 9:
-        arraia.classList.add('ativo')
-        arraia.classList.remove('desativo')
-      break
-
-      case 10:
-        coruja.classList.add('ativo')
-        coruja.classList.remove('desativo')
+        axolote.classList.remove('ativo')
+      axolote.classList.add('desativo')
       break
 
   }
